5d07b94459e84d0635f1b04e79d02bf121bc8ee0
Andrew Smith <smitha110@winthrop.edu>
2017-02-08 15:53:11 -0500

    Initial template for GPS tracking.

---
+++ HelloWorldTemplate/platforms/browser/www/js/gps.js
@@ -0,0 +1,24 @@
+document.addEventListener("deviceready", onDeviceReady, false);
+	function onDeviceReady(){
+		//onSuccess Callback
+			//This method accepts a Position object, which contains the current GPS coords.
+		var onSuccess = function(position){
+			alert(
+					'Latitude: ' + position.coords.latitude + '\n' +
+					'Longitude: ' + position.coords.longitude + '\n' +
+					'Altitude: ' + position.coords.altitude + '\n' +
+					'Accuracy: ' + position.coords.accuracy + '\n' +
+					'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '\n' +
+					'Heading: ' + position.coords.heading + '\n' +
+					'Speed: ' + position.coords.speed + '\n' +
+					'Timestamp: ' + position.timestamp '\n'
+				);
+		};
+
+		function onError(error){
+			alert('code: ' + error.code + '\n' +
+				'message: ' + error.message + '\n'
+				);
+		};
+		navigator.geolocation.getCurrentPosition(onSuccess, onError);
+	}
